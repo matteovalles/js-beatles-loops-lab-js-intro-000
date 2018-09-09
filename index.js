62e7c79a28dbf29e2b2e1747d2c55aa0413afd78
Matteo Valles <matteovalles@gmail.com>
2018-09-09 23:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(person, instrument) {
+  var arr = []
+  for (let i = 0, i < person.length, i++) {
+    arr.push(`${person} plays ${instrument}`)
+  }
+  return arr
+}
